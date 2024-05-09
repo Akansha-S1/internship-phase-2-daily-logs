4355ee8c4a88426e277c03f449c02da68378a3e5
akansha-shetty <akanshashetty07@gmail.com>
2024-05-09 10:20:59 +0530

    Updating the solutions

--- DAY-11/discussion-8-solution.sql
+++ DAY-11/discussion-8-solution.sql
@@ -1,155 +1,38 @@
--- Query 1: Find the addresses of all departments
-select 
-    d.location_id,
-    l.street_address,
-    l.city,
-    l.state_province,
-    c.country_name
-from 
-    departments d
-join 
-    locations l on d.location_id = l.location_id
-join 
-    countries c on l.country_id = c.country_id;
+-- 1.Write a query to find the addresses (location_id, street_address, city, state_province, country_name) of all the departments.
+    select d.department_name,l.location_id,l.street_address,l.city,l.state_province,c.country_name from locations l inner join countries c on c.country_id=l.country_id inner join departments d on d.location_id=l.location_id group by d.department_id;
 
--- Query 2: Find the name, department ID, and department name of all employees
-select 
-    concat(e.first_name, ' ', e.last_name) as name,
-    e.department_id,
-    d.department_name
-from 
-    employees e
-join 
-    departments d on e.department_id = d.department_id;
+-- 2.Write a query to find the name (first_name, last name), department ID, and department name of all the employees.
+    select concat(e.first_name," ",e.last_name) as employee_name,e.department_id,d.department_name from employees e inner join departments d on d.department_id=e.department_id group by e.employee_id;
 
--- Query 3: Find the name, job, department ID of employees who work in London
-select 
-    concat(e.first_name, ' ', e.last_name) as name,
-    j.job_title,
-    e.department_id
-from 
-    employees e
-join 
-    jobs j on e.job_id = j.job_id
-join 
-    departments d on e.department_id = d.department_id
-join 
-    locations l on d.location_id = l.location_id
-where 
-    l.city = 'London';
+-- 3.Write a query to find the name (first_name, last_name), job, department ID, and name of the employees who work in London.
+    select concat(e.first_name," ",e.last_name) as employee_name,j.job_title,d.department_id from employees e inner join jobs j using(job_id) inner join departments d using (department_id) inner join locations l using(location_id) where l.city='London';
 
--- Query 4: Find employee ID, name, manager ID, and manager name
-select 
-    e1.employee_id,
-    e1.last_name,
-    e1.manager_id,
-    e2.last_name as manager_name
-from 
-    employees e1
-join 
-    employees e2 on e1.manager_id = e2.employee_id;
+-- 4.Write a query to find the employee id, name (last_name) along with their manager_id, and name (last_name).
+    select e.employee_id,e.last_name,e.manager_id,e1.last_name from employees e inner join employees e1 on e.manager_id=e1.employee_id;
 
--- Query 5: Find employees hired after 'Jones'
-select 
-    concat(first_name, ' ', last_name) as name,
-    hire_date
-from 
-    employees
-where 
-    hire_date > (select hire_date from employees where last_name = 'Jones');
+-- 5.Write a query to find the name (first_name, last_name) and hire date of the employees who were hired after 'Jones'.
+    select concat(e.first_name," ",e.last_name) as employee_name,e.hire_date from employees e inner join employees e1 on e1.last_name='Jones' where e.hire_date>e1.hire_date;
 
--- Query 6: Get department name and number of employees in each department
-select 
-    d.department_name,
-    count(*) as no_of_employees
-from 
-    employees e
-join 
-    departments d on e.department_id = d.department_id
-group by 
-    d.department_name;
+-- 6.Write a query to get the department name and number of employees in the department.
+    select d.department_name,count(e.employee_id) as number_of_employees from departments d inner join employees e on e.department_id=d.department_id group by department_name;
 
--- Query 7: Get job ID, name, and number of days between start and end date for jobs in department 90
-select 
-    j.job_id,
-    j.job_title,
-    datediff(jh.end_date, jh.start_date) as days_between
-from 
-    job_history jh
-join 
-    jobs j on jh.job_id = j.job_id
-where 
-    j.department_id = 90;
+-- 7.Write a query to find the employee ID, job title, number of days between the ending date and the starting date for all jobs in department 90.
+    select employee_id,job_title,end_date-start_date as number_of_days from job_history natural join jobs where department_id=90;
 
--- Query 8: Display department ID, manager name, and manager's first name
-select 
-    d.department_id,
-    concat(e1.first_name, ' ', e1.last_name) as manager_name,
-    e2.first_name as manager_first_name
-from 
-    employees e1
-join 
-    employees e2 on e1.manager_id = e2.employee_id
-join 
-    departments d on e1.department_id = d.department_id;
+-- 8.Write a query to display the department ID and name and first name of the manager.
+    select d.department_id,d.department_name,e.first_name as manager_name from departments d inner join employees e on d.manager_id=e.manager_id;
 
--- Query 9: Display department name, manager name, and city
-select 
-    d.department_name,
-    concat(e.first_name, ' ', e.last_name) as manager_name,
-    l.city
-from 
-    employees e
-join 
-    departments d on e.department_id = d.department_id
-join 
-    locations l on d.location_id = l.location_id
-where 
-    e.employee_id = d.manager_id;
+-- 9.Write a query to display the department name, manager name, and city.
+    select d.department_name,concat(e.first_name," ",e.last_name) as manager_name,l.city from departments d inner join employees e on d.manager_id=e.employee_id inner join locations l using(location_id);
 
--- Query 10: Display job title and average salary of employees
-select 
-    j.job_title,
-    avg(e.salary) as average_salary
-from 
-    employees e
-join 
-    jobs j on e.job_id = j.job_id
-group by 
-    j.job_title;
+-- 10.Write a query to display the job title and average salary of employees.
+    select job_title,round(avg(salary),2) as average_salary from employees inner join jobs using(job_id) group by job_title;
 
--- Query 11: Display job title, employee name, and salary difference from minimum salary for the job
-select 
-    j.job_title,
-    concat(e.first_name, ' ', e.last_name) as employee_name,
-    e.salary - j.min_salary as salary_difference
-from 
-    employees e
-join 
-    jobs j on e.job_id = j.job_id;
+-- 11.Write a query to display job title, employee name, and the difference between the salary of the employee and minimum salary for the job.
+    select job_title,concat(first_name," ",last_name) as employee_name,(salary-min_salary) as difference_of_salary from employees inner join jobs using(job_id);
 
--- Query 12: Display job history of employees currently earning more than $10000
-select 
-    jh.*
-from 
-    job_history jh
-join 
-    employees e on jh.employee_id = e.employee_id
-where 
-    e.salary > 10000;
+-- 12.Write a query to display the job history of any employee who is currently drawing more than 10000 of salary.
+    select jh.* from job_history jh inner join employees e on jh.employee_id=e.employee_id where e.salary>10000;
 
--- Query 13: Display department name, manager name, hire date, and salary of managers with more than 15 years of experience
-select 
-    d.department_name,
-    concat(e.first_name, ' ', e.last_name) as manager_name,
-    jh.hire_date,
-    e.salary
-from 
-    employees e
-join 
-    departments d on e.department_id = d.department_id
-join 
-    job_history jh on e.employee_id = jh.employee_id
-where 
-    e.employee_id = d.manager_id
-    and datediff(jh.end_date, jh.start_date) > 15;
+-- 13.Write a query to display department name, name (first_name, last_name), hire date, the salary of the manager for all managers whose experience is more than 15 years.
+    select d.department_name,concat(e.first_name, " ",e.last_name) as employee_name,e.hire_date,e.salary from employees e inner join departments d using(department_id) inner join job_history jh using(employee_id) where e.employee_id=d.manager_id and end_date-start_date>15;
